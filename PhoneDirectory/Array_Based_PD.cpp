bf99915dd691929a5fab687865170b3d69244725
gbingham18 <gbingham18@my.whitworth.edu>
2016-09-23 10:49:18 -0700

    Made a change to remove_entry function

--- PhoneDirectory/Array_Based_PD.cpp
+++ PhoneDirectory/Array_Based_PD.cpp
@@ -122,7 +122,7 @@ string Phone_Directory::remove_entry(const string& name) // Exercise 1.7: please
 		{
 			the_directory[i] = the_directory[i + 1];
 		}
-		size = size - 1;
+		return the_directory[index].get_number();
 	}
 	// Hint: you can use the code below to shift names down in the directory to remove the selected entry specified by "index"
 	// for (int i = index; i < size - 1; i++)
